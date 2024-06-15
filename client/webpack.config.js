cdcc7cf64beb9bf123e3d10e32f6319dc35b43a0
walker <spadonival7@gmail.com>
2024-06-15 14:49:12 -0400

    refactored client webpack.config.js and install.js and started npm run start to start server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,11 +19,51 @@ module.exports = () => {
     },
     plugins: [
       
+      new HtmlWebpackPlugin({        
+        template: './index.html',
+        title: 'Webpack Plugin'
+    }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'JATE',
+      short_name: 'JATE',
+      description: 'Keep track of important notes!',
+      background_color: '#7eb4e2',
+      theme_color: '#7eb4e2',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+      
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        },
+          {
+            test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+          }
+        }
+      }
       ],
     },
   };
